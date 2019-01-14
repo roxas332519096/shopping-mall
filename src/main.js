9b2b83cbb92ac396163b98a8c9952c9803888b8b
roxas332519096 <roxas332519096@vip.qq.com>
2019-01-14 22:12:25 +0800

    add home/nav-bar

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import swiper from 'swiper'
 
 Vue.config.productionTip = false
 Vue.$axios = axios;
-
+Vue.$swiper = swiper;
 
 new Vue({
+  el:'#app',
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+})
+
